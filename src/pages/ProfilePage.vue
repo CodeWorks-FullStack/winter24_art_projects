ffd3e36971fffcfb2aae20a1cb17af24dd7baabb
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-05 11:07:40 -0700

    getting profile with id from route param

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,5 +1,27 @@
 <script setup>
+import { profilesService } from '@/services/ProfilesService.js';
+import { logger } from '@/utils/Logger.js';
+import Pop from '@/utils/Pop.js';
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 
+// NOTE gives us information about the current route in the router-view
+const route = useRoute()
+
+onMounted(() => {
+  getProfileById()
+})
+
+async function getProfileById() {
+  try {
+    // NOTE pulls the profile id out of the url
+    const profileId = route.params.profileId
+    await profilesService.getProfileById(profileId)
+  } catch (error) {
+    Pop.meow(error)
+    logger.error('[GETTING PROFILE BY ID]', error)
+  }
+}
 </script>
 
 
