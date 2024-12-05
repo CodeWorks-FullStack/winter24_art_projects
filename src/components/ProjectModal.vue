71d2d25ec69157d356822d9c024500e5665eac2e
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-05 10:37:19 -0700

    showing images in project modal

--- src/components/ProjectModal.vue
+++ src/components/ProjectModal.vue
@@ -8,15 +8,21 @@ const project = computed(() => AppState.activeProject)
 
 
 <template>
+  <!-- NOTE bootstrap still needs these two upper divs to open the modal -->
   <div class="modal fade" id="projectModal" tabindex="-1" aria-labelledby="projectModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-      <div class="modal-content">
+      <!-- NOTE do not show the inner contents of the modal until there is a project in the appstate -->
+      <div v-if="project != null" class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="projectModalLabel">{{ project?.title }}</h1>
+          <h1 class="modal-title fs-5" id="projectModalLabel">
+            {{ project.title }} by {{ project.creator.name }}
+          </h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          ...
+          <div v-for="projectImg in project.projectImgs" :key="projectImg">
+            <img :src="projectImg" alt="">
+          </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -27,4 +33,8 @@ const project = computed(() => AppState.activeProject)
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>
